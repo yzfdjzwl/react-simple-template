aae542dfcc713866d6fbff89113bc4b09949ec6a
余正峰 <yuzhengfeng@meituan.com>
2018-06-07 23:44:07 +0800

    docs: modify webpack.config.js docs

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -13,14 +13,29 @@ module.exports = {
   },
   devServer: {
     // if you don't set contentBase, the root of the server resource is the root of the project
-    // if you set like this: contentBase: path.resolve(__dirname, './dist'),
+    // the base config of contentBase like this: `contentBase: path.resolve(__dirname)`
+    // if you set like this: `contentBase: path.resolve(__dirname, './dist')`
     // the root of the server resource is the `dist`
-    // server will find `index.html` in `the root of the server resource`
+
     compress: true,
+
     // webpack-dev-server will use `localhost:8888` as a service
     port: 8888,
+
+    // server will find `index.html` in `the root of the server resource`
     // webpack-dev-server will return `index.html` when enter `localhost:8888`
-    // that's why: //github.com/expressjs/express/issues/3664
+    // that's why: https://github.com/expressjs/express/issues/3664
+    // * on [`webpack-dev-server`](https://github.com/webpack/webpack-dev-server/blob/master/lib/Server.js#L311)
+    // * the code is `app.get('*', express.static(contentBase, options.staticOptions));`
+    // * `express.static()` use third-party libraries: [serve-static](https://github.com/expressjs/serve-static).
+    // * `serve-static` use third-party libraries: [send](https://github.com/pillarjs/send).
+    // * In [send/index.js](https://github.com/pillarjs/send/blob/master/index.js#L591), when path is `'/'`, it's will return `index.html`.
+
+    // config the default home page
+    // webpack-dev-server will return `app.html` when enter `localhost:8888`
+    staticOptions: {
+      index: 'app.html'
+    },
   },
   module: {
     rules: [
