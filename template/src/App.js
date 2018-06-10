bbe2b0b264550e3188d43d9e4580d844a0e74a53
余正峰 <yuzhengfeng@meituan.com>
2018-06-10 23:15:41 +0800

    chore: update webpack config

--- template/src/App.js
+++ template/src/App.js
@@ -3,7 +3,11 @@ import './index.css';
 import pic from './webpack.svg';
 
 class App extends Component {
+
   render() {
+    if (process.env.NODE_ENV === 'development') {
+      console.log(`This is development type.`);
+    }
     return (
       <div className="container">
         <div className="text">Hello, Welcome you to use my template..</div>
--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
+  // webpack4 config
+  // value: development | production | none
+  // default value: production
+  // if the value is `production`, `process.env.NODE_ENV` will be replaced with `production` by `webpack` in `src`
+  // if the value is `development`, `process.env.NODE_ENV` will be replaced with `development` by `webpack` in `src`
+  mode: 'none',
+
   entry: [
     './src/index.js',
-  ]
+  ],
   output: {
     // The output dist path
     path: path.resolve(__dirname, './dist'),
@@ -14,6 +22,10 @@ module.exports = {
     publicPath: '/dist/',
   },
   devServer: {
+    // webpack-dev-server ouput files exists in memory
+    // webpack ouput files exists in disk
+
+
     // if you don't set contentBase, the root of the server resource is the root of the project
     // the base config of contentBase like this: `contentBase: path.resolve(__dirname)`
     // if you set like this: `contentBase: path.resolve(__dirname, './dist')`
@@ -23,6 +35,9 @@ module.exports = {
 
     // webpack-dev-server will use `localhost:8888` as a service
     port: 8888,
+
+    // for spa and `404`, it rely on HTML5 history API, if set true, all the jumps will refer to `index.html`
+    historyApiFallback: true,
 
     // server will find `index.html` in `the root of the server resource`
     // webpack-dev-server will return `index.html` when enter `localhost:8888`
@@ -35,9 +50,9 @@ module.exports = {
 
     // config the default home page
     // webpack-dev-server will return `app.html` when enter `localhost:8888`
-    staticOptions: {
-      index: 'app.html'
-    },
+    // staticOptions: {
+    //   index: 'app.html'
+    // },
   },
   module: {
     rules: [
@@ -62,3 +77,26 @@ module.exports = {
     ]
   },
 };
+
+
+// `cross-env` can set and use environment variables across platforms
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map';
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"',
+      },
+    }),
+  ]);
+}
+
+if (process.env.NODE_ENV === 'development') {
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"development"',
+      },
+    }),
+  ]);
+}
